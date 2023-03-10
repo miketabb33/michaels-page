c5498db355cbacf0563e1324377d5f1921dc37d0
miketabb33 <miketabb33@gmail.com>
2023-03-09 22:07:18 -0500

    add linting and typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,21 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
-  plugins: [new HtmlWebpackPlugin({ template: 'public/index.html'})],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [{ test: /\.([cm]?ts|tsx)$/, loader: 'ts-loader' }],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: 'public/index.html' })],
+
   devServer: {
     static: {
       directory: path.join(__dirname, 'public'),
